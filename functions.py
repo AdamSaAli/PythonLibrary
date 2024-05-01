ce6a893ddd030c062892fd464b1f595efdd82020
Adam Ali <adam@ecosinfo.com>
2024-05-01 14:40:29 -0400

    Now when the user selects the unit they want it takes them to another window to promt the date they want the data to be fetched from

--- functions.py
+++ functions.py
@@ -4,13 +4,21 @@ from customtkinter import *
 app = CTk()
 app.geometry("500x400")
 
-def getUserData():
-    print(checkbox.get())
+def getUserDate():
+ 
+    new=CTkToplevel(app)
+    textbox=CTkEntry(master=new,placeholder_text="Input date in the form of xx/xxx/xxxx to xx/xxx/xxxx")
+    textbox.pack(anchor="center",expand=True,pady=100)
+    #Double check the way the date is actually stored
+    print("hihihih")
+    
+
 checkbox = CTkCheckBox(master=app,text="Choose which unit(s)")
-button = CTkButton(master=app,text="submit",command=getUserData)
+button = CTkButton(master=app,text="submit",command=getUserDate)
 # I need to somehow add more boxes to this depending on how many units the user has
 checkbox.place(relx=0.5,rely=0.5,anchor="center")
-button.pack(anchor="n",expand=True)
+button.pack(anchor="n",expand=True,)
 #I will probably need  a function thats called when the user submits the amount of units they want that can fetch the data needed
 
+#in this setup the window that asks how many unit sthey want and the window that asks the date range they want are both open, maybe I should close the first one after getting how many units they want or maybe I should keep it open in case they want to change which units they have selected
 app.mainloop()
