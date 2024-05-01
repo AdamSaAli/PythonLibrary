cd03d56780020c01cd77a0dc1e83674e76548ef1
Adam Ali <adam@ecosinfo.com>
2024-05-01 13:50:12 -0400

    Got a rough idea on how the gui will look

--- functions.py
+++ functions.py
@@ -1,15 +1,16 @@
 from customtkinter import *
 
+#I think I should rename this file as it mostly has the gui stuff the user interacts with
 app = CTk()
 app.geometry("500x400")
-def user_input():
-    print(entry.get())
 
-entry = CTkEntry(master=app, placeholder_text="Input",width=300)
-button = CTkButton(master=app,text="submit",command=user_input)
-#entry.place(relx=0.5,rely=0.5,anchor="center")
-
-entry.pack(anchor="s",expand=True,pady=10)
+def getUserData():
+    print(checkbox.get())
+checkbox = CTkCheckBox(master=app,text="Choose which unit(s)")
+button = CTkButton(master=app,text="submit",command=getUserData)
+# I need to somehow add more boxes to this depending on how many units the user has
+checkbox.place(relx=0.5,rely=0.5,anchor="center")
 button.pack(anchor="n",expand=True)
+#I will probably need  a function thats called when the user submits the amount of units they want that can fetch the data needed
 
 app.mainloop()
