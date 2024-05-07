8f3be3fb5b3b99f80dadbfaef93c1dc8fb055a22
Adam Ali <adam@ecosinfo.com>
2024-05-07 13:13:45 -0400

    going to save incase but we decided to go with something else

--- functions.py
+++ functions.py
@@ -3,6 +3,11 @@ from pymongo import MongoClient
 import urllib.parse
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+#from gui import getUserPollutant
+#import pprint
+
+#maybe I should import pprint to print the data in a nicer format
+
 user = urllib.parse.quote_plus('adam2')
 pas = urllib.parse.quote_plus('hl5jsBQVe7mIlO6V')
 uri = f"mongodb+srv://adam:Work123!@#cluster0.f6mndat.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
@@ -16,9 +21,32 @@ collection=mydb["FakeCID"]
 
 
 def getPollutant():
+    #pollutant = getUserPollutant()
+    #This will work when I tie the gui with the functions  
+    pollutant = "PM"
+    #the spelling is very important so make sure to double check that
+    res=collection.find({"Sensor":f"{pollutant}"})
+    print(list(res))
     
-res=collection.find({"Sensor":"Gas"})
-print(res)
-for hello in res:
-    print(hello)
+    return res
+    #I will probably need to send this back to gui file to display it
 
+def getSpecificTime():
+    
+    return
+
+def getSpecificUnit():
+    #this needs to be able to return more than one unit if need be
+    #collection=mydb["FakeCID"] I am going to have to be able to insert all the units the user could want into the FakeCID spot
+    #Im thinking in the gui file when returning the users units I store them in a list and return that
+    #then I could iterate over the list and insert those into the spot 
+    return
+
+#res=collection.find({"Sensor":"Gas"})
+#print(res)
+#for hello in res:
+#    print(hello)
+
+hi = getPollutant()
+#print(hi)
+#print("HIHIHIHI")
--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 from customtkinter import *
 
-#I think I should rename this file as it mostly has the gui stuff the user interacts with
+
 app = CTk()
 app.geometry("500x400")
 
@@ -12,7 +12,7 @@ def getUserDate():
     textbox.pack(anchor="center",expand=True,pady=100)
     #Double check the way the date is actually stored
     submitDate = CTkButton(master=dateWindow,text="submit",command=getUserPollutant)
-    submitDate.pack(anchor="n",expand=True)
+    submitDate.pack(anchor="center",expand=True)
     #make the window nicer by changin colours, centering stuff better and just improve the overall appearance 
     return
 # not done with this function
@@ -22,10 +22,12 @@ def getUserPollutant():
     PollutantWindow.title("This is the window to retrieve the Pollutant")
     gasCheckBox=CTkCheckBox(master=PollutantWindow,text="Gas")
     PMCheckBox=CTkCheckBox(master=PollutantWindow,text="PM")
+    #maybe I should use a drop down menu rather than checkboxes so the user can only chose one option
     gasCheckBox.pack(anchor="n")
     PMCheckBox.pack(anchor="n")
     PollutantSubmit = CTkButton(master=PollutantWindow,text="submit")
     PollutantSubmit.pack(anchor="n")
+    
     return
 #not done with this function
 
@@ -34,8 +36,7 @@ checkbox = CTkCheckBox(master=app,text="Choose which unit(s)")
 
 button = CTkButton(master=app,text="submit",command=getUserDate)
 # I need to somehow add more boxes to this depending on how many units the user has
-#checkbox.place(relx=0.5,rely=0.5,anchor="center")
-checkbox.pack(anchor="n",expand=True)
+checkbox.pack(anchor="center",expand=True)
 button.pack(anchor="n",expand=True)
 
 #I will probably need  a function thats called when the user submits the amount of units they want that can fetch the data needed
