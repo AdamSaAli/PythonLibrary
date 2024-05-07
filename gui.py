493a71452a3a4a2b0292142ccdefacdb40cacf84
Adam Ali <adam@ecosinfo.com>
2024-05-07 13:33:48 -0400

    made 3 seperate frames, I am planning on putting each specific option we need from the user into each one of these frames

--- gui.py
+++ gui.py
@@ -3,43 +3,21 @@ from customtkinter import *
 
 app = CTk()
 app.geometry("500x400")
+set_appearance_mode("dark")
+app.title("User input")
 
-def getUserDate():
- 
-    dateWindow=CTkToplevel(app)
-    dateWindow.title("This is the window to retrieve the date")
-    textbox=CTkEntry(master=dateWindow,placeholder_text="Input date in the form of xx/xxx/xxxx to xx/xxx/xxxx")
-    textbox.pack(anchor="center",expand=True,pady=100)
-    #Double check the way the date is actually stored
-    submitDate = CTkButton(master=dateWindow,text="submit",command=getUserPollutant)
-    submitDate.pack(anchor="center",expand=True)
-    #make the window nicer by changin colours, centering stuff better and just improve the overall appearance 
-    return
-# not done with this function
+frame1=CTkFrame(master=app,fg_color="red",height=20,width=20)
+frame1.pack()
 
-def getUserPollutant():
-    PollutantWindow=CTkToplevel(app)
-    PollutantWindow.title("This is the window to retrieve the Pollutant")
-    gasCheckBox=CTkCheckBox(master=PollutantWindow,text="Gas")
-    PMCheckBox=CTkCheckBox(master=PollutantWindow,text="PM")
-    #maybe I should use a drop down menu rather than checkboxes so the user can only chose one option
-    gasCheckBox.pack(anchor="n")
-    PMCheckBox.pack(anchor="n")
-    PollutantSubmit = CTkButton(master=PollutantWindow,text="submit")
-    PollutantSubmit.pack(anchor="n")
-    
-    return
-#not done with this function
+Checkbox
 
+frame2=CTkFrame(master=app,fg_color="blue")
+frame2.pack()
 
-checkbox = CTkCheckBox(master=app,text="Choose which unit(s)")
+frame3 = CTkFrame(master=app,fg_color="white")
+frame3.pack(expand=True)
+pollutant =CTkComboBox(master=frame3,values=["PM","Gas"])
+#maybe we would like to add specific PM's or a specific gas
+pollutant.pack(expand=True)
 
-button = CTkButton(master=app,text="submit",command=getUserDate)
-# I need to somehow add more boxes to this depending on how many units the user has
-checkbox.pack(anchor="center",expand=True)
-button.pack(anchor="n",expand=True)
-
-#I will probably need  a function thats called when the user submits the amount of units they want that can fetch the data needed
-
-#in this setup the window that asks how many unit sthey want and the window that asks the date range they want are both open, maybe I should close the first one after getting how many units they want or maybe I should keep it open in case they want to change which units they have selected
 app.mainloop()
